f4ced80cebd7d92c0c00de1120bd0951436c9fcf
noorcheg <81104881+noorcheg@users.noreply.github.com>
2022-01-12 18:38:18 +0500

    create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+let i = 1000;
+const deadInside = () => {
+    setTimeout(() => {
+            console.log(${i} - 7 = ${i - 7});
+            i -= 7;
+            if (i > 0) {
+                deadInside();
+            } else {
+                console.log('Kek');
+            }
+    }, 50);
+}
+
+deadInside();
