5dbef54c4e3f82e3d00f44428da9cf2d90bd049b
Ainur Galiakberov <noorcheg@gmail.com>
2022-01-15 00:48:53 +0500

    fix: quotes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let i = 1000;
 const deadInside = () => {
     setTimeout(() => {
-            console.log(${i} - 7 = ${i - 7});
+            console.log(`${i} - 7 = ${i - 7}`);
             i -= 7;
             if (i > 0) {
                 deadInside();
